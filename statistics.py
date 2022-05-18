ec2d3da19cf85eaea0646ba95e09cd5957ee7492
SinthujaRavichandran <40533860+SinthujaRavichandran@users.noreply.github.com>
2022-05-18 15:00:04 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,13 +1,12 @@
 import math
 
 def calculateStats(numbers):
-  
   try:
-        tot = 0
-        for i in numbers:
-            tot = tot+i
-        return {"avg":float(tot)/len(numbers), "max":max(numbers), "min":min(numbers)}
-   except ZeroDivisionError as e:
-        return {"avg":math.nan, "max":math.nan, "min":math.nan}
+    tot = 0
+    for i in numbers:
+      tot = tot+i
+    return {"avg":float(tot)/len(numbers), "max":max(numbers), "min":min(numbers)}
+  except Exception as e:
+    return {"avg":math.nan, "max":math.nan, "min":math.nan}
        
   #return None
